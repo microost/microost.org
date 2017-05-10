462662e1bbc5c98d006623c669db89b6a36df2da
Dai MIKURUBE <dmikurube@acm.org>
2017-05-10 21:06:18 +0900

    Unshallow the cloned Git repository in Travis CI.

--- check_packages.py
+++ check_packages.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
+import os
+import subprocess
 import sys
 
 
+def get_git_dir():
+    git_dir = subprocess.check_output(['git', 'rev-parse', '--git-dir'])
+    return os.path.abspath(git_dir)
+
+
+def unshallow_git_if_shallow():
+    subprocess.check_call(['git', 'config', 'remote.origin.fetch', '"+refs/heads/*:refs/remotes/origin/*"'])
+    if os.path.exists(os.path.join(get_git_dir(), 'shallow')):
+        subprocess.check_call(['git', 'fetch', '--unshallow'])
+
+
 def main(argv):
+    unshallow_git_if_shallow()
     print 'Done.'
 
 
